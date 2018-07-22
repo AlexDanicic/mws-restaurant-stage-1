1a92a44eb3d071cd46c89f67bef13992d87b1b62
Acke83 <alex.danicic@gmail.com>
2018-07-22 14:16:03 +0100

    Service Worker fixed, offline maps are loading and fetch properly

--- sw.js
+++ sw.js
@@ -1,110 +1,57 @@
-var staticCacheName = 'restaurant-cahce-1';
-let urlToCache = [
-    '/skeleton',
-    'js/main.js',
-    'js/restaurant_info.js',
-    'js/dbhelper.js',
-    'css/styles.css',
-    'data/restaurants.json',
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-    'img/6.jpg',
-    'img/7.jpg',
-    'img/8.jpg',
-    'img/9.jpg',
-    'img/10.jpg',
-    '//normalize-css.googlecode.com/svn/trunk/normalize.css"',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-];
-self.addEventListener('install', function (event) {
-    console.log('work fine');
+const files = [
+    '/',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/restaurant.html',
+    '/js/restaurant_info.js',
+    '/js/dbhelper.js',
+    '/js/main.js',
+  
+  ];
+  
+  const staticCacheName = 'restaurant-static-v2';
+  
+  
+  //cache files
+  self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(urlToCache);
-        })
+      caches.open(staticCacheName).then(function (cache) {
+        return cache.addAll(files)
+      })
     );
-});
-
-self.addEventListener('activate', function (event) {
+  });
+  
+  
+  //update files
+  self.addEventListener('activate', function (event) {
     event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('restaurant-') &&
-                        cacheName != staticCacheName;
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
+      caches.keys().then(function (cacheNames) {
+        return Promise.all(cacheNames.map(cacheName => {
+          if(cacheName !== staticCacheName) {
+            caches.delete(cacheName);
+          }
+        }))
+  
+      })
     );
-});
-
-self.addEventListener('fetch', function (event) {
+  })
+  
+  
+  self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(e => console.log(e))
     );
-});
-
-// const staticCacheName = 'restaurant-cahce-1';
-
-// let urlToCache = [
-//     '/',
-//     './restaurant.html',
-//     './css/styles.css',
-//     './data/restaurants.json',
-//     './img/1.jpg',
-//     './img/2.jpg',
-//     './img/3.jpg',
-//     './img/4.jpg',
-//     './img/5.jpg',
-//     './img/6.jpg',
-//     './img/7.jpg',
-//     './img/8.jpg',
-//     './img/9.jpg',
-//     './img/10.jpg',
-//     './js/main.js',
-//     './js/restaurant_info.js',
-//     './js/dbhelper.js',
-//     './index.html'
-// ];
-// self.addEventListener('install', function (event) {
-
-//     event.waitUntil(
-//         caches.open(staticCacheName).then(function (cache) {
-//             console.log(cache);
-//             return cache.addAll(urlToCache);
-
-//         }).catch(error => {
-//             console.log(error);
-//         })
-//     );
-// });
-
-// self.addEventListener('activate', function (event) {
-//     event.waitUntil(
-//         caches.keys().then(function (cacheNames) {
-//             return Promise.all(
-//                 cacheNames.filter(function (cacheName) {
-//                     return cacheName.startsWith('restaurant-') &&
-//                         cacheName != staticCacheName;
-//                 }).map(function (cacheName) {
-//                     return caches.delete(cacheName);
-//                 })
-//             );
-//         })
-//     );
-// });
-
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function (response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
+  });
