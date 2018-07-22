a420dca618e1134928e73469edb4e322f0028957
Acke83 <alex.danicic@gmail.com>
2018-07-22 14:08:51 +0100

    sw.js tested

--- sw.js
+++ sw.js
@@ -1,34 +1,29 @@
 var staticCacheName = 'restaurant-cahce-1';
-
 let urlToCache = [
-    '/',
-    './restaurant.html',
-    './css/styles.css',
-    './data/restaurants.json',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './js/dbhelper.js',
-    './index.html'
+    '/skeleton',
+    'js/main.js',
+    'js/restaurant_info.js',
+    'js/dbhelper.js',
+    'css/styles.css',
+    'data/restaurants.json',
+    'img/1.jpg',
+    'img/2.jpg',
+    'img/3.jpg',
+    'img/4.jpg',
+    'img/5.jpg',
+    'img/6.jpg',
+    'img/7.jpg',
+    'img/8.jpg',
+    'img/9.jpg',
+    'img/10.jpg',
+    '//normalize-css.googlecode.com/svn/trunk/normalize.css"',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
 ];
 self.addEventListener('install', function (event) {
-
+    console.log('work fine');
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
-            console.log(cache);
             return cache.addAll(urlToCache);
-
-        }).catch(error => {
-            console.log(error);
         })
     );
 });
@@ -55,3 +50,61 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
+
+// const staticCacheName = 'restaurant-cahce-1';
+
+// let urlToCache = [
+//     '/',
+//     './restaurant.html',
+//     './css/styles.css',
+//     './data/restaurants.json',
+//     './img/1.jpg',
+//     './img/2.jpg',
+//     './img/3.jpg',
+//     './img/4.jpg',
+//     './img/5.jpg',
+//     './img/6.jpg',
+//     './img/7.jpg',
+//     './img/8.jpg',
+//     './img/9.jpg',
+//     './img/10.jpg',
+//     './js/main.js',
+//     './js/restaurant_info.js',
+//     './js/dbhelper.js',
+//     './index.html'
+// ];
+// self.addEventListener('install', function (event) {
+
+//     event.waitUntil(
+//         caches.open(staticCacheName).then(function (cache) {
+//             console.log(cache);
+//             return cache.addAll(urlToCache);
+
+//         }).catch(error => {
+//             console.log(error);
+//         })
+//     );
+// });
+
+// self.addEventListener('activate', function (event) {
+//     event.waitUntil(
+//         caches.keys().then(function (cacheNames) {
+//             return Promise.all(
+//                 cacheNames.filter(function (cacheName) {
+//                     return cacheName.startsWith('restaurant-') &&
+//                         cacheName != staticCacheName;
+//                 }).map(function (cacheName) {
+//                     return caches.delete(cacheName);
+//                 })
+//             );
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(
+//         caches.match(event.request).then(function (response) {
+//             return response || fetch(event.request);
+//         })
+//     );
+// });
